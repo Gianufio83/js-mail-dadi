d7706b67adc751839bf639d8f0a97b828b1f6597
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-07 18:11:33 +0100

    esercizio dadi completo

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,17 +1,18 @@
 // alert('ciao');
 // Gioco dei dadi, chi fa di più vince
 alert('tira i dadi');
-// document.getElementById("primo_dado").innerHTML = first;
 var first = Math.floor(Math.random() * 7);
+// document.getElementById("primo_dado").innerHTML = first;
 console.log(first);
-// document.getElementById("secondo_dado").innerHTML = second;
 var second = Math.floor(Math.random() * 7);
+// document.getElementById("secondo_dado").innerHTML = second;
 console.log(second);
 
-var somma = first + second;
-console.log(somma);
+var sommaUno = first + second;
+console.log(sommaUno);
 
 alert('Ricorda il numero che hai totalizzato tirando i dadi e ritira di nuovo i dadi');
+
 var third = Math.floor(Math.random() * 7);
 console.log(third);
 var fourth = Math.floor(Math.random() * 7);
@@ -20,10 +21,12 @@ console.log(fourth);
 var secondoTiro = third + fourth;
 console.log(secondoTiro);
 
-if (somma >= secondoTiro) {
+if (sommaUno > secondoTiro) {
   console.log('Hai vinto');
-} else if (somma = secondoTiro) {
+}
+else if (sommaUno == secondoTiro) {
   console.log ('Riprova');
-} else {
+}
+else {
   console.log('Hai perso');
 }
